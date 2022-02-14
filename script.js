5d5b09fa5cbaaaa33d62c99c6066479b859b4ef1
Emma Bowden <emzie.bowden@gmail.com>
2022-02-14 22:10:49 +0000

    Update function to call API instead of array

--- script.js
+++ script.js
@@ -1,66 +1,50 @@
-console.log(results);
+//query for the button
+let button = document.querySelector(".header__button");
 
 //------LINK TO API-------//
 // const SANDWICH_API =
 //   "https://api.spoonacular.com/recipes/complexSearch?apiKey=8f633cb4cc0d42a2b1be3ec9d3740667&query=sandwich&number=30&type=main course&addRecipeInformation=true";
 
-//------CALL THE API-------//
-
-//query for the button
-let button = document.querySelector(".header__button");
-
-//add event listener to button
+//------ADD EVENT LISTENER-------//
 button.addEventListener("click", (event) => {
   displayRecipe(event);
 });
 
+//------CREATE ARRAYS FOR DISPLAY RECIPE FUNCTION-------//
 let veggieArray = [];
 let meatyArray = [];
 
+//------CALL THE API-------//
 function displayRecipe(event) {
   event.preventDefault();
 
-  //   axios
-  //     .get(SANDWICH_API)
-  //     .then((response) => {
-  //       //call the data
-  //       let dataArray = response.data.results;
-  //       console.log(dataArray);
-  //       //pick a random recipe
-  //       let randomRecipe = randomIndex(dataArray);
-  //       console.log(randomIndex(dataArray));
-  //       //generate the content via DOM
-  //       createSandwich(randomRecipe);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  
-  button.textContent = "nah, try again!";
-  dietPreference(results);
+  axios
+    .get(SANDWICH_API)
+    .then((response) => {
+      //call the data
+      let dataArray = response.data.results;
 
-  //function to check radio selection
-  if (document.getElementById("either").checked) {
-    let randomRecipe = randomIndex(results);
-    createSandwich(randomRecipe);
-    container.innerHTML = "";
-  } else if (document.getElementById("meaty").checked) {
-    let randomRecipe = randomIndex(meatyArray);
-    createSandwich(randomRecipe);
-    container.innerHTML = "";
-  } else if (document.getElementById("veggie").checked) {
-    let randomRecipe = randomIndex(veggieArray);
-    createSandwich(randomRecipe);
-    container.innerHTML = "";
-  }
+      button.textContent = "nah, try again!";
+      dietPreference(dataArray);
 
-  //   //USING THE SANDWICH.JS NOT CALLING API
-  //   //pick a random recipe
-  //   let randomRecipe = randomIndex(results);
-  //   console.log(randomIndex(results));
-  //   //generate the content via DOM
-  //   createSandwich(randomRecipe);
-  //   container.innerHTML = "";
+      //function to check radio selection
+      if (document.getElementById("either").checked) {
+        let randomRecipe = randomIndex(dataArray);
+        createSandwich(randomRecipe);
+        container.innerHTML = "";
+      } else if (document.getElementById("meaty").checked) {
+        let randomRecipe = randomIndex(meatyArray);
+        createSandwich(randomRecipe);
+        container.innerHTML = "";
+      } else if (document.getElementById("veggie").checked) {
+        let randomRecipe = randomIndex(veggieArray);
+        createSandwich(randomRecipe);
+        container.innerHTML = "";
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 //------FUNCTIONS-------//
@@ -106,16 +90,11 @@ function createSandwich(recipe) {
 
 //function to create veg/meat arrays
 function dietPreference(array) {
-  //   let veggieArray = [];
-  //   let meatyArray = [];
-
   for (let i = 0; i < array.length; i++) {
     if (array[i].vegetarian === true) {
       veggieArray.push(array[i]);
-      //   console.log(veggieArray);
     } else {
       meatyArray.push(array[i]);
-      //   console.log(meatyArray);
     }
   }
   return { veggieArray, meatyArray };
@@ -125,3 +104,38 @@ function dietPreference(array) {
 // https://stackoverflow.com/questions/20677997/javascript-break-string-on-based-on-preceding-consecutive-characters
 // https://spoonacular.com/food-api/console#Dashboard
 // https://www.geeksforgeeks.org/how-to-get-character-array-from-string-in-javascript/
+
+//------RETIRED CODE-------//
+
+// console.log(dataArray);
+// //pick a random recipe
+// let randomRecipe = randomIndex(dataArray);
+// console.log(randomIndex(dataArray));
+// //generate the content via DOM
+// createSandwich(randomRecipe);
+
+// button.textContent = "nah, try again!";
+// dietPreference(results);
+
+// //function to check radio selection
+// if (document.getElementById("either").checked) {
+//   let randomRecipe = randomIndex(results);
+//   createSandwich(randomRecipe);
+//   container.innerHTML = "";
+// } else if (document.getElementById("meaty").checked) {
+//   let randomRecipe = randomIndex(meatyArray);
+//   createSandwich(randomRecipe);
+//   container.innerHTML = "";
+// } else if (document.getElementById("veggie").checked) {
+//   let randomRecipe = randomIndex(veggieArray);
+//   createSandwich(randomRecipe);
+//   container.innerHTML = "";
+// }
+
+//   //USING THE SANDWICH.JS NOT CALLING API
+//   //pick a random recipe
+//   let randomRecipe = randomIndex(results);
+//   console.log(randomIndex(results));
+//   //generate the content via DOM
+//   createSandwich(randomRecipe);
+//   container.innerHTML = "";
